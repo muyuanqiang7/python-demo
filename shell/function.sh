8a9a2d8c6fda331696a7fdac382d5bd22f265d46
muyuanqiang7 <muyuanqiang7@gmail.com>
2018-01-30 10:02:18 +0800

    修改函数添加文件包含

--- shell/function.sh
+++ shell/function.sh
@@ -12,11 +12,11 @@
 #}
 
 echoFunc(){
-    echo "this is first funcrion in shell"
+    echo "this is first function in shell"
 }
-echo "-----------函数开始执行---------"
-echoFunc
-echo "-----------函数执行完毕---------"
+#echo "-----------函数开始执行---------"
+#echoFunc
+#echo "-----------函数执行完毕---------"
 
 
 returnFunc(){
@@ -46,7 +46,7 @@ paramFunc(){
     echo "第八个参数 $8"
 }
 
-paramFunc 1 2 3 4 5 6 7 $param
+#paramFunc 1 2 3 4 5 6 7 $param
 # $10 不能获取第十个参数，获取第十个参数需要${10}。当n>=10时，需要使用${n}来获取参数。
 # $#	传递到脚本的参数个数
 # $*	以一个单字符串显示所有向脚本传递的参数
@@ -58,4 +58,33 @@ paramFunc 1 2 3 4 5 6 7 $param
 
 
 ## shell输入/输出重定向
+# 标准输入文件(stdin)：stdin的文件描述符为0，Unix程序默认从stdin读取数据。
+# 标准输出文件(stdout)：stdout 的文件描述符为1，Unix程序默认向stdout输出数据。
+# 标准错误文件(stderr)：stderr的文件描述符为2，Unix程序会向stderr流中写入错误信息。
+# 默认情况下，command > file 将 stdout 重定向到 file，command < file 将stdin 重定向到 file。
+# 表示标准错误文件(stderr)。
 
+# 如果希望将 stdout 和 stderr 合并后重定向到 file，可以这样写：
+
+# $ command > file 2>&1
+# 或者
+# $ command >> file 2>&1
+#
+
+# /dev/null 文件
+# 如果希望执行某个命令，但又不希望在屏幕上显示输出结果，那么可以将输出重定向到 /dev/null：
+
+# $ command > /dev/null
+# /dev/null 是一个特殊的文件，写入到它的内容都会被丢弃；如果尝试从该文件读取内容，那么什么也读不到。
+# 但是 /dev/null 文件非常有用，将命令的输出重定向到它，会起到"禁止输出"的效果。
+
+# 如果希望屏蔽 stdout 和 stderr，可以这样写：
+
+# $ command > /dev/null 2>&1
+
+# 0 是标准输入（STDIN），1 是标准输出（STDOUT），2 是标准错误输出（STDERR）。
+
+who > ./users
+
+read text < ./users
+echo $text
---
+++ shell/invoke.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# shell 文件包涵 引入外部文件
+#. filename   # 注意点号(.)和文件名中间有一空格
+#或
+# source filename
+#. ./function.sh
+
+source ./function.sh
+echoFunc
